9a37ad7b9589591d0991837092aef9fc9050a7b3
문태진 <mtj1015@naver.com>
2024-05-02 21:04:42 +0900

    [Feat] MainFragment 연동 추가
    [Feat] - 기능 추가
    [Fix] - 기능 수정
    [UI] - xml파일 건드림

---
+++ app/src/main/java/com/tedMoon/clone_daangnproject/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.tedMoon.clone_daangnproject
+
+import android.os.Bundle
+import android.os.SystemClock
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.transition.MaterialSharedAxis
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
